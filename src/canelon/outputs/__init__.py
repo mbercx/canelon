c74a16d72e8b2062d8d1206d79acdbf474ec7c0b
Marnik Bercx <mbercx@gmail.com>
2026-04-30 10:05:59 +0200

    ✨ Initial commit from package template

---
+++ src/canelon/outputs/__init__.py
@@ -0,0 +1,5 @@
+"""Public output classes."""
+
+from canelon.outputs.demo import DemoOutput
+
+__all__ = ["DemoOutput"]
---
+++ src/canelon/outputs/parsers/__init__.py
@@ -0,0 +1,3 @@
+from canelon.outputs.parsers.json_parser import JSONParser
+
+__all__ = ["JSONParser"]
